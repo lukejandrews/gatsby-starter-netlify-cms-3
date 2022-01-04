67ccfdd96bee13f3e0d34adcdb4cab7caf0b5d5e
lukejandrews <87306715+lukejandrews@users.noreply.github.com>
2022-01-04 17:47:04 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,8 +78,22 @@ module.exports = {
       options: {
         develop: true, // Activates purging in npm run develop
         purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+     {
+      resolve: `gatsby-plugin-clarity`,
+      options: {
+        // String value for your clarity project id
+        // Project id is found in your clarity dashboard url
+        // https://clarity.microsoft.com/projects/view/{clarity_project_id}/
+        clarity_project_id: YOUR_PROJECT_ID,
+        // Boolean value for enabling clarity while developing
+        // true will enable clarity tracking code on both development and production environments
+        // false will enable clarity tracking code on production environment only
+        //
+        enable_on_dev_env: true,
       },
+    },
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
+   
 };
